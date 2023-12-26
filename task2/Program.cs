8e2223d0b0c9097d909c8bc27058bad146f62dea
Tatyana <tanusik91@inbox.ru>
2023-12-27 02:08:59 +0700

    add some sum and pr lesson01:task1

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = i1 + l1;
+Console.WriteLine(sum2);
+
+double d1 = 4.5;
+double d2=4.5;
+double sum3 = d2 + d1;
+Console.WriteLine(sum3);
+
+int pr1=i1*i2;
+long pr2 =i1*l1;
+double pr3=i1*d1;
+Console.WriteLine(pr1);
+Console.WriteLine(pr2);
+Console.WriteLine(pr3);
