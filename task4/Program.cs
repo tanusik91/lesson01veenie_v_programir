665666af2933e2a33e10c2ef77e7f04aa3beab48
Tatyana <tanusik91@inbox.ru>
2023-12-27 18:25:27 +0700

    add lession01:task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 81;
+int b = 7;
+int c = 12;
+int d = 45;
+int e = 11;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
